14c3995d85796e6f8e5db3096996bcc52982f8df
tharun <tharungo@gmail.com>
2022-02-13 21:31:45 +0530

    first

---
+++ src/main/webapp/WEB-INF/jsp/login.jsp
@@ -0,0 +1,16 @@
+<html>
+
+<title>
+</title>
+<body>
+
+<form method ="GET">
+ ${msg }
+ UserNameTT:<input type = "text" value ="" name ="name"><br>
+ Password:<input type ="password" name ="password">
+ <input type = "submit" name = "submit" />
+ <a href="/hello" />
+</form>
+</body>
+
+</html>
---
+++ src/test/java/com/rakesh/springboot/firstwebapp/FirstWebappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rakesh.springboot.firstwebapp;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertEquals(1, 1);
+	}
+
+}
